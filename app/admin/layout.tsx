6c582fbbd74a4d0b58d51e7b47f9677cb4d266b0
hadeberonald <ronald.v.hadebe@gmail.com>
2026-01-04 00:31:50 +0200

    fixes

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,4 +1,5 @@
 'use client';
+export const dynamic = 'force-dynamic';
 
 import ProtectedRoute from '../../components/auth/ProtectedRoute';
 
--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,4 +1,5 @@
 'use client';
+export const dynamic = 'force-dynamic';
 
 import { useState, useEffect } from 'react';
 import ProductCard from '../../components/ProductCard';
