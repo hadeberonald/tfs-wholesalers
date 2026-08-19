e6507d4a19a60ccadb34c9714c0e604a6c51cd0e
raydanon <raymdhadebe@gmail.com>
2026-08-19 12:58:05 +0200

    Rename Wholesale Promotion to Non Foods Promotion on Dundee bot

--- whatsapp-bot/src/data/menus.js
+++ whatsapp-bot/src/data/menus.js
@@ -66,7 +66,7 @@ module.exports = {
         title: "Promotions",
         rows: [
           { id: "retail_promo", title: "Retail Promotion", description: "" },
-          { id: "wholesale_promo", title: "Wholesale Promotion", description: "" },
+          { id: "wholesale_promo", title: "Non Foods Promotion", description: "" },
           { id: "main_menu", title: "Main Menu", description: "" },
         ],
       },
@@ -85,7 +85,7 @@ module.exports = {
     retail_promo:
       "Sorry, the retail promotion isn't available right now. Please check back soon.",
     wholesale_promo:
-      "Sorry, the wholesale promotion isn't available right now. Please check back soon.",
+      "Sorry, the non foods promotion isn't available right now. Please check back soon.",
     order:
       "Welcome to the TFS Vryheid orders & deliveries on WhatsApp 📝🚗\n\n" +
       "Please give us your name so we know who we're speaking to.\n\n" +
